d3d4277a537c9323694ad375430ce4e1c320e7a1
simonlopez25 <simonlopezsaldarroaga@gmail.com>
2026-07-10 20:14:37 +0200

    feat(nav): create navigation bar with design and functionality

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("html/nav.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("worldNavbar").innerHTML = data;
+    })
+    .catch((error) => console.error("Error al cargar el menú:", error));
+});
