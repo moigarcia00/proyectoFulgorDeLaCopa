71f7c11e120c98f051467de12e814940d1a04e17
simonlopez25 <simonlopezsaldarroaga@gmail.com>
2026-07-16 09:49:24 +0200

    feat(nav): implement dynamic active class for navigation links

--- js/nav.js
+++ js/nav.js
@@ -3,6 +3,19 @@ document.addEventListener("DOMContentLoaded", () => {
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("worldNavbar").innerHTML = data;
+
+      const path = window.location.pathname;
+      const navLinks = document.querySelectorAll(".navItem");
+
+      navLinks.forEach((link) => {
+        const linkHref = link.getAttribute("href");
+
+        if (path.includes(linkHref.replace("../", ""))) {
+          link.classList.add("active");
+        } else {
+          link.classList.remove("active");
+        }
+      });
     })
     .catch((error) => console.error("Error al cargar el menú:", error));
 });
